ac5f6da3a4c422092f7680c2acf6e185c15779ae
Hegde07 <hegdebhargav92@gmail.com>
2023-08-29 22:44:11 -0700

    Fixed URL error

--- src/pages/CheckoutForm.js
+++ src/pages/CheckoutForm.js
@@ -61,7 +61,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: `https://e-commerce-backend-xi.vercel.app//order-success/${currentOrder.id}`,
+        return_url: `https://e-commerce-backend-xi.vercel.app/order-success/${currentOrder.id}`,
       },
     });
 
